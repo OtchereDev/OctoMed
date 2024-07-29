b0e0dd85eb04654cef653c9a8a8b349cef3449f4
Oliver Otchere <oliverotchere4@gmail.com>
2024-07-29 20:29:29 +0100

    added dto for onboarding

--- frontend/app/dto/user.dto.ts
+++ frontend/app/dto/user.dto.ts
@@ -45,9 +45,43 @@ export const ResetPasswordDTO = z
     path: ['confirm_password'],
   })
 
+export const BioDataDTO = z.object({
+  avatar: z.string(),
+  height: z.number({ message: 'Provide a height' }),
+  height_metric: z.string(),
+  weight: z.number({ message: 'Provide a weight' }),
+  weight_metric: z.string(),
+})
+
+export const HealthDetailDTO = z.object({
+  allergies: z.array(z.number()),
+  health_conditions: z.array(z.number()),
+  remove_allergies: z.array(z.number()),
+  remove_health_conditions: z.array(z.number()),
+})
+
+export const LocationDTO = z.object({
+  city: z.string().optional(),
+  country: z.string().min(1, { message: 'Country is required' }),
+  name: z.string().min(5, { message: 'Emergency contact name is required' }),
+  phone_number: z.string().regex(/^\+?[1-9]\d{8,14}$/, { message: 'Phone number is required' }),
+  region: z.string().min(1, { message: 'Region is required' }),
+  street: z.string().optional(),
+})
+
 type SignupDTOType = z.infer<typeof SignupDTO>
 type LoginDTOType = z.infer<typeof LoginDTO>
 type ForgotPasswordDTOType = z.infer<typeof ForgotPasswordDTO>
+type BioDataDTOType = z.infer<typeof BioDataDTO>
+type HealthDetailDTOType = z.infer<typeof HealthDetailDTO>
+type LocationDTOType = z.infer<typeof LocationDTO>
 
 export { ForgotPasswordDTO, LoginDTO, SignupDTO }
-export type { ForgotPasswordDTOType, LoginDTOType, SignupDTOType }
+export type {
+  BioDataDTOType,
+  ForgotPasswordDTOType,
+  HealthDetailDTOType,
+  LocationDTOType,
+  LoginDTOType,
+  SignupDTOType,
+}

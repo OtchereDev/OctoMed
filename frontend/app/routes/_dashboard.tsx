7d6f00c8f4ccf1d9bb795a107709ff9e942de641
Oliver Otchere <oliverotchere4@gmail.com>
2024-07-29 20:19:40 +0100

    completed new dashboard layout looks

--- frontend/app/routes/_dashboard.tsx
+++ frontend/app/routes/_dashboard.tsx
@@ -1,6 +1,7 @@
 import { ActionFunctionArgs, LoaderFunctionArgs, json } from '@remix-run/node'
-import { Outlet, redirect } from '@remix-run/react'
+import { Link, Outlet, redirect } from '@remix-run/react'
 import { useState } from 'react'
+import SideImage from '~/assets/images/dashboard-sidebar.svg'
 import SignoutButton from '~/components/shared/Signout'
 import {
   Bell,
@@ -15,6 +16,13 @@ import {
   More,
   User,
 } from '~/components/shared/icons'
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTrigger,
+} from '~/components/ui/drawer'
 import { preventUnAuthorizedUser } from '~/lib/preventUnAuthorizedUser'
 import { commitSession, getSession } from '~/sessions'
 
@@ -28,8 +36,6 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 export async function action({ request }: ActionFunctionArgs) {
   const session = await getSession(request.headers.get('Cookie'))
-
-  console.log('I was called')
 
   session.unset('accessToken')
   session.unset('email')
@@ -57,15 +63,7 @@ const links = [
     title: 'Health Care Providers',
     link: '/health-care-providers',
   },
-  {
-    Icon: More,
-    title: 'More',
-    link: '#',
-  },
-]
 
-const DesktopLinks = [
-  ...links.slice(0, 3),
   {
     Icon: Diet,
     title: 'Diet and Exercises',
@@ -88,15 +86,17 @@ export default function DashboardLayout() {
   return (
     <section className="max-h-screen w-full lg:grid lg:grid-cols-[280px,auto]">
       <section className="hidden h-screen overflow-scroll lg:block">
-        <aside className="relative hidden h-full w-[280px] flex-col border-r px-6 py-[30px] lg:flex">
-          <div className="text-primary">
-            <Logo className="mb-[63px] h-[27.22px] w-[115px] pl-[6px]" />
+        <aside className="relative hidden h-full w-[280px] flex-col border-r pb-[30px] lg:flex">
+          <div className="relative flex items-center overflow-hidden px-6 pb-[20px] pt-[20px] text-white">
+            <img src={SideImage} className="absolute left-0 top-0 w-full" />
+            <Logo className="relative h-[27.22px] w-[115px] pl-[10px]" />
           </div>
 
-          <div className="flex flex-1 flex-col justify-between">
+          <div className="mt-[40px] flex flex-1 flex-col justify-between px-6">
             <div className="flex flex-col gap-5">
-              {DesktopLinks.map(({ link, Icon, title }) => (
+              {links.map(({ link, Icon, title }) => (
                 <div
+                  key={title}
                   className={`flex cursor-pointer items-center gap-3 rounded-full bg-opacity-20 px-[24px] py-[10px] ${selected == title ? 'bg-[#1282A2] font-semibold text-primary' : 'text-[#4D5061]'}`}
                 >
                   <Icon />
@@ -106,19 +106,13 @@ export default function DashboardLayout() {
             </div>
 
             <div className="border-t pt-[30px] font-montserrat font-medium text-[#4D5061]">
-              {/* <Form method="POST">
-                <button type="submit" className="flex items-center gap-3">
-                  <Signout />
-                  <p>Sign Out</p>
-                </button>
-              </Form> */}
               <SignoutButton />
             </div>
           </div>
         </aside>
       </section>
       <section className="relative flex flex-col lg:h-screen lg:min-h-screen lg:px-10">
-        <nav className="fixed left-0 top-0 flex w-full items-center justify-between border-b border-[#D0D5DD] bg-white px-7 py-5 lg:relative lg:px-0">
+        <nav className="fixed left-0 top-0 z-10 flex w-full items-center justify-between border-b border-[#D0D5DD] bg-white px-7 py-5 lg:relative lg:px-0">
           <LogoSM className="h-[27.22px] w-[35px] lg:hidden" />
           <p className="hidden font-montserrat text-xl font-semibold text-[#333] lg:block">
             Welcome, Oliver 😊
@@ -126,18 +120,20 @@ export default function DashboardLayout() {
 
           <div className="flex items-center gap-5">
             <Bell className="size-6 cursor-pointer text-gray-500" />
-            <div className="flex h-7 w-7 cursor-pointer items-center justify-center rounded-full border border-gray-600 text-gray-500">
-              <User className="size-5" />
-            </div>
+            <Link to={'/profile'}>
+              <div className="flex h-7 w-7 cursor-pointer items-center justify-center rounded-full border border-gray-600 text-gray-500">
+                <User className="size-5" />
+              </div>
+            </Link>
           </div>
         </nav>
 
-        <section className="flex-1 overflow-scroll pt-20 lg:pt-0">
+        <section className="flex-1 overflow-scroll pb-32 pt-20 lg:pt-0">
           <Outlet />
         </section>
 
         <aside className="fixed bottom-0 left-0 flex w-full justify-between border-t bg-white px-[30px] py-[17px] lg:hidden">
-          {links.map(({ Icon, title, link }) => (
+          {links.slice(0, 3).map(({ Icon, title, link }) => (
             <div key={title} className="flex cursor-pointer flex-col items-center">
               <Icon />
               <p
@@ -147,6 +143,28 @@ export default function DashboardLayout() {
               </p>
             </div>
           ))}
+          <Drawer>
+            <DrawerTrigger>
+              <div className="flex cursor-pointer flex-col items-center">
+                <More />
+                <p className={`mt-[13px] font-poppins text-[10px]`}>More</p>
+              </div>
+            </DrawerTrigger>
+            <DrawerContent>
+              <DrawerHeader>
+                <DrawerDescription className="flex flex-col divide-y">
+                  {links.slice(3).map(({ Icon, title, link }) => (
+                    <Link to={link} className="pb-4">
+                      <div className="mt-3 flex items-center gap-4 rounded-lg px-3 py-4 font-montserrat hover:bg-gray-100">
+                        <Icon />
+                        <p>{title}</p>
+                      </div>
+                    </Link>
+                  ))}
+                </DrawerDescription>
+              </DrawerHeader>
+            </DrawerContent>
+          </Drawer>
         </aside>
       </section>
     </section>
